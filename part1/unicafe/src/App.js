ea3d71a898d1e612b738fd41183388013e41badd
cesarhenrq <cesaarhvb@gmail.com>
2023-07-11 02:55:38 -0300

    Exercise 1.8: unicafe step3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -12,11 +12,7 @@ const Statistic = ({ text, value }) => (
   </p>
 );
 
-const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-
+const Statistics = ({ good, neutral, bad }) => {
   const total = good + neutral + bad;
 
   const average = (good - bad) / total;
@@ -25,10 +21,6 @@ const App = () => {
 
   return (
     <div>
-      <Title text='give feedback' />
-      <Button text='good' handleClick={() => setGood(good + 1)} />
-      <Button text='neutral' handleClick={() => setNeutral(neutral + 1)} />
-      <Button text='bad' handleClick={() => setBad(bad + 1)} />
       <Title text='statistics' />
       <Statistic text='good' value={good} />
       <Statistic text='neutral' value={neutral} />
@@ -38,6 +30,23 @@ const App = () => {
       {!isNaN((good / total) * 100) && (
         <Statistic text='positive' value={positive} />
       )}
+    </div>
+  );
+};
+
+const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+
+  return (
+    <div>
+      <Title text='give feedback' />
+      <Button text='good' handleClick={() => setGood(good + 1)} />
+      <Button text='neutral' handleClick={() => setNeutral(neutral + 1)} />
+      <Button text='bad' handleClick={() => setBad(bad + 1)} />
+      <Title text='statistics' />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
   );
 };
