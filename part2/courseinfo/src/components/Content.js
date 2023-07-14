5db32473304026d73c175ff51148495251448939
cesarhenrq <cesaarhvb@gmail.com>
2023-07-14 01:48:17 -0300

    Exercise 2.2: Course information step7

--- part2/courseinfo/src/components/Content.js
+++ part2/courseinfo/src/components/Content.js
@@ -1,13 +1,17 @@
 import React from "react";
 
 import Part from "./Part";
+import Total from "./Total";
 
 const Content = ({ parts }) => {
+  const total = parts.reduce((acc, part) => acc + part.exercises, 0);
+
   return (
     <>
       {parts.map((part) => (
         <Part part={part} key={part.id} />
       ))}
+      <Total total={total} />
     </>
   );
 };
