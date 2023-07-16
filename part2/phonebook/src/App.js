6a6ff41999ad5c202304939681c7d84065888a39
cesarhenrq <cesaarhvb@gmail.com>
2023-07-16 19:17:53 -0300

    Exercise 2.7: The Phonebook Step2

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -40,6 +40,11 @@ const App = () => {
       name: newName,
     };
 
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+      return;
+    }
+
     setPersons(persons.concat(person));
 
     setNewName("");
