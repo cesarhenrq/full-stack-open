f98e0d14b70bf886c766e9c48a01fa5407e5dbee
cesarhenrq <cesaarhvb@gmail.com>
2023-07-20 01:17:43 -0300

    Exercise 2.12: The Phonebook step7

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,65 +1,67 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect } from "react";
 
-import axios from 'axios';
+import axios from "axios";
 
-import Title from './components/Title';
+import Title from "./components/Title";
 
-import Filter from './components/Filter';
+import Filter from "./components/Filter";
 
-import FormPerson from './components/FormPerson';
+import FormPerson from "./components/FormPerson";
 
-import Persons from './components/Persons';
+import Persons from "./components/Persons";
 
 const App = () => {
   const [persons, setPersons] = useState([]);
 
   const [newContact, setNewContact] = useState({
-    name: '',
-    number: '',
+    name: "",
+    number: "",
   });
 
-  const [filter, setFilter] = useState('');
+  const [filter, setFilter] = useState("");
 
   useEffect(() => {
-    axios.get('http://localhost:3001/persons').then(response => {
+    axios.get("http://localhost:3001/persons").then((response) => {
       setPersons(response.data);
     });
   }, []);
 
-  const handleChange = event =>
+  const handleChange = (event) =>
     setNewContact({ ...newContact, [event.target.name]: event.target.value });
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
 
     const person = {
       ...newContact,
     };
 
-    if (persons.some(person => person.name === newContact.name)) {
+    if (persons.some((person) => person.name === newContact.name)) {
       alert(`${newContact.name} is already added to phonebook`);
       return;
     }
 
-    setPersons(persons.concat(person));
+    axios.post("http://localhost:3001/persons", person).then((response) => {
+      setPersons(persons.concat(response.data));
 
-    setNewContact({
-      name: '',
-      number: '',
+      setNewContact({
+        name: "",
+        number: "",
+      });
     });
   };
 
   return (
     <div>
-      <Title text="Phonebook" />
+      <Title text='Phonebook' />
       <Filter value={filter} onChange={setFilter} />
-      <Title text="Add a new" />
+      <Title text='Add a new' />
       <FormPerson
         onSubmit={handleSubmit}
         fields={newContact}
         onChange={handleChange}
       />
-      <Title text="Numbers" />
+      <Title text='Numbers' />
       <Persons persons={persons} filter={filter} />
     </div>
   );
